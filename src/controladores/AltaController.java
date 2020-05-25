943797b40916b5a582fa2a93e8fcec36af0a5bbe
LeleMX <LeleMC@outlook.es>
2020-05-25 01:31:00 -0700

    Progreso de alta empleado

--- src/controladores/AltaController.java
+++ src/controladores/AltaController.java
@@ -1,4 +1,34 @@
 package controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+
 public class AltaController {
+    @FXML
+    private TextField Textrfc, Textnombre, Textedad, Texttelefono, Textemail;
+    @FXML
+    private RadioButton Rm, Rf;
+    @FXML
+    private SplitMenuButton Splitpuestos;
+
+
+    public void generarEmpleado() {
+        String rfc, nombre, telefono, email, puesto;
+        int edad;
+        boolean sexo;
+
+        rfc = Textrfc.getText();
+        nombre = Textnombre.getText();
+        if (Rm.isSelected()) {
+            sexo = true;
+        } else {
+            sexo = false;
+        }
+        telefono = Texttelefono.getText();
+        edad = Integer.parseInt(Textedad.getText());
+        email = Textemail.getText();
+
+    }
 }
